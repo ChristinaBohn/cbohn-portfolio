1f35cadb06c46148cfba83fbf1669609448f1aaf
Christina Bohn <christinajbohn@gmail.com>
2022-05-08 09:50:52 -0700

    Added images and links for collaborative projects on portfolio page.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, {useState} from 'react';
 import './App.css';
-import ResponsiveAppBar from './components/AppBar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+// Import Pages & Components
+import ResponsiveAppBar from './components/AppBar';
 import Portfolio from './pages/Portfolio/index';
 import AboutMe from './pages/AboutMe';
 import Footer from './components/Footer';
 import Contact from './pages/Contact';
 import Resumé from './pages/Resume';
 
+
 function App() {
 const pages = ['About', 'Portfolio', 'Contact', 'Resumé'];
 const [currentPage, setCurrentPage] = useState(pages[0])
   
-
   return (
     <Router>
 
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -4,17 +4,54 @@ import React from 'react';
 import {
     Container,
     Grid,
-    Typography,
-    Paper
+    Link,
+    Paper,
+    Typography
 } from '@mui/material';
 
 // Image Imports
-import dailyPlanner from './images/daily-planner-.png';
+import dailyPlanner from './images/daily-planner.png';
+import dogeared from './images/dogeared.png';
+import selfExploration from './images/self-exploration.png';
+import myTeamStats from './images/myTeamStats.png'
 
 // Project Porfolio
 export default function Portfolio() {
     return (
             <Container maxWidth="lg">
+
+                <Grid container spacing={3}>
+
+                    <Typography variant='h1'>Collaborations</Typography>
+
+                    <Link underline="none" href='https://myteam-stats.herokuapp.com/'>    
+                        <Grid item xs>
+                            <Paper style={{height: "300px"}} elevation={16}>
+                                <img src={myTeamStats} alt="My Team Stats preview" height="300px"></img>
+                            </Paper>
+                        </Grid>
+                    </Link>
+
+                    <Link underline="none" href='https://dogeared-2022.herokuapp.com/login'> 
+                        <Grid item xs>
+                            <Paper style={{height: "300px"}} elevation={16}>
+                                <img src={dogeared} alt="Dogeared preview" height="300px"></img>
+                            </Paper>
+                        </Grid>
+                    </Link>
+
+                    <Link underline="none" href='https://mikiwolfe.github.io/30-day-Challenge/'> 
+                        <Grid item xs>
+                            <Paper style={{height: "300px"}} elevation={16}>
+                                <img src={selfExploration} alt="40 Day Challenge preview" height="300px"></img>
+                            </Paper>
+                        </Grid>
+                    </Link>
+
+                </Grid>
+
+                <br></br>
+
                 <Grid container spacing={3}>
 
                     <Typography variant='h1'>My Projects</Typography>
