0e9bfeda7490d432fe670b75fb0f9e22013aedfd
Christina Bohn <christinajbohn@gmail.com>
2022-05-15 13:54:09 -0700

    Began technologies list on Resume page.

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -2,12 +2,22 @@ import * as React from 'react';
 
 // MUI Imports
 import { 
+  Avatar,
   Button,
   Container,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
   Paper,
   Typography
 } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
+
+
+import HtmlIcon from '@mui/icons-material/Html';
+import CssIcon from '@mui/icons-material/Css';
+import JavascriptIcon from '@mui/icons-material/Javascript';
 
 
 // Resume Download & Technology Icons
@@ -20,12 +30,49 @@ export default function Resume() {
 
         <Paper elevation={16} square>
 
-            {/* TODO: Add icons for technologies and download resume button */}
-            <Button variant="outlined" startIcon={<DownloadIcon />} href="https://docs.google.com/document/d/1-Ez6R4vPEDNbmoVxvZX2iN5IfzmdmrrESedbttqQDAw/edit" target="_blank" size="large">
-              Resume
-            </Button>
+            {/* TODO: Map over these, add rest of technologies, use MUI virtual list for scrolling? */}
+            <List>
+
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <HtmlIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Hypertext Markup Language"
+                  />
+                </ListItem>
+
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <CssIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Cascading Style Sheets"
+                  />
+                </ListItem>
+
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <JavascriptIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Javascript"
+                  />
+                </ListItem>
+            
+            </List>
 
         </Paper>
+
+        <Button variant="outlined" startIcon={<DownloadIcon />} href="https://docs.google.com/document/d/1-Ez6R4vPEDNbmoVxvZX2iN5IfzmdmrrESedbttqQDAw/edit" target="_blank" color="inherit" size="large">
+              Resume
+        </Button>
 
     </Container>
 
